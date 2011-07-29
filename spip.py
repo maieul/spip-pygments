5e3fc32b1ed7dfd1466b622f18ced29f744d7e72
= <maieul@maieul.net>
2011-07-29 14:58:48 +0200

    debut des boucles, avec les critères, reste à la colorer

--- spip.py
+++ spip.py
@@ -9,15 +9,17 @@ class SPIPLexer(RegexLexer):
 	
 	tokens = {
 		'root': [
+			
+			
 			(r'#[A-Z_]+\*{0,2}', Keyword),								# balises
 			(r'\|[\w:]+',Name.Function),								# filtres
 			(r'<:[\w]+(:[\w]+)?:>',Literal.String.Other,'recur'),		# chaînes de langues simple (sans filtres dedans)
 			(r':>',Literal.String.Other),								# fin chaîne de langue
 			(r'<:[\w]+(:[\w]+)?',Literal.String.Other),					# début chaine
-			(r'\{[#\s\w,!=?<>:]+\}',Name.Attribute,'recur'),				# Critères de boucles et arguments de balises ainsi que de filtres
-			(r'<\/{0,2}B(OUCLE)?[\w]+>',Name.Class),						# partie optionelle des boucles
-			(r'<BOUCLE[\w]+\([\w]+\)>',Name.Class)
-			
+			(r'\{[#\s\w,!=?<>:]+\}',Name.Attribute,'recur'),			# Critères de boucles et arguments de balises ainsi que de filtres
+			(r'<\/{0,2}B(OUCLE)?[\w]+>',Name.Class),					# partie optionelle des boucles et fermeture
+			(r'<BOUCLE[\w]+\([\w]+\)([\s]*\{[#\s\w,\|!=?<>:\{\}]+\}[\s]*)*>',Name.Class),						# ouverture de la boucle
+
 		],
 		
 		'recur':[
