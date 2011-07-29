f16ac1087ca482e4c25c12e72de12553b24370b1
= <maieul@maieul.net>
2011-07-29 16:34:02 +0200

    j'ai enfin compris comment cela marche : on lit presque caractère à caractère, du coup je peux simplifier le code + debuguer pour les critères imbriqués

--- spip.py
+++ spip.py
@@ -9,15 +9,16 @@ class SPIPLexer(RegexLexer):
 	
 	tokens = {
 		'root': [
-			(r'(<BOUCLE[\w]+\([\w]+\))([\s]*\{[#\s\w,\|!=?<>:\{\}]+\}[\s]*)*(>)',bygroups(Name.Class,Name.Attribute,Name.Class)),						# ouverture de la boucle
+			(r'(<BOUCLE[\w]+\([\w]+\))([\s]*\{[#\s\w,\|!=?<>:\{\}]+\}[\s]*)*(>)',bygroups(Name.Class,Name.Attribute,Name.Class)),								# ouverture de la boucle
 			
 			(r'#[A-Z_]+\*{0,2}', Keyword),								# balises
 			(r'\|[\w:]+',Name.Function),								# filtres
-			(r'<:[\w]+(:[\w]+)?:>',Literal.String.Other,'recur'),		# chaînes de langues simple (sans filtres dedans)
+			(r'<:[\w]+(:[\w]+)?:>',Literal.String.Other),				# chaînes de langues simple (sans filtres dedans)
 			(r':>',Literal.String.Other),								# fin chaîne de langue
 			(r'<:[\w]+(:[\w]+)?',Literal.String.Other),					# début chaine
-			(r'\{[#\s\w,!=?<>:]+\}',Name.Attribute,'recur'),			# Critères de boucles et arguments de balises ainsi que de filtres
 			(r'<\/{0,2}B(OUCLE)?[\w]+>',Name.Class),					# partie optionelle des boucles et fermeture
+			(r'\{',Name.Attribute,'critere'),										#debut d'un critères
+			(r'\}',Name.Attribute),										#fin d'un critères
 			
 
 		],
@@ -26,17 +27,16 @@ class SPIPLexer(RegexLexer):
 		#	(r'<BOUCLE[\w]+\([\w]+\)',Name.Class),						# debut boucle
 		#],
 		
-		'recur':[
+		'critere':[
 			
 			(r'#[A-Z_]+\*{0,2}', Keyword),								# balise
-			(r'\{[\s\w,!=?]+\}',Name.Attribute,'#push'), 				# critères
 			(r'\|[\w:]+',Name.Function),								# filtres
-			(r'<:[\w]+(:[\w\{\}=\|]+)?:>',Literal.String.Other,'recur'),#chaînes de langues
 			(r':>',Literal.String.Other),								# fin chaîne de langue
 			(r'<:[\w]+(:[\w]+)?',Literal.String.Other),					# début chaine
 
-			(r'\}',Name.Attribute),										#fin d'un critères
-			(r'\{[\s\w,!=?<>:]+',Name.Attribute),						#debut d'un critères
+			(r'\}',Name.Attribute,'#pop'),								#fin d'un critères
+			(r'[\w=,]+',Name.Attribute),								# contenu simple d'un critères
+			(r'\{',Name.Attribute,'critere'),						#debut d'un critères
 			
 
 		],
